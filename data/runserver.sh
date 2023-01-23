5a1927bfc8965627413a6333470635f43cc4b55e
Mohsen Jamalifar <mo.j@tuta.io>
2023-01-23 19:30:55 +0330

    server sh

--- data/runserver.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-nano /etc/apt/sources.list
-deb https://mirror.iranserver.com/debian bullseye main contrib non-free
-apt update
-apt install task-xfce-desktop
-apt install xrdp
-apt install network-manager-openvpn
-adduser 
