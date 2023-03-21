513f63a1473b978a25833add1700a6f8efdf8fef
Mohsen Jamalifar <mo.j@tuta.io>
2023-03-21 08:50:57 +0330

    new layout

--- container_0/tether_check/analyze/tether/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.utils import timezone
-
-class Ticker(models.Model):
-    price = models.FloatField(default=0)
-    time = models.DateTimeField(default=timezone.now)
