99c99b92925b9a0eaf70787922f9da9d17f48448
Mohsen Jamalifar <mohsen.j.far@gmail.com>
2024-03-15 08:45:40 +0000

    update

--- user_data/strategies/hold_strategy.py
+++ user_data/strategies/hold_strategy.py
@@ -5,6 +5,9 @@ from typing import Optional, Union, Tuple
 from datetime import datetime, timedelta
 from freqtrade.persistence import Trade
 import talib.abstract as ta
+from technical import qtpylib
+from sklearn.cluster import KMeans
+from sklearn.linear_model import LinearRegression
 
 from freqtrade.strategy import (
     IStrategy,
@@ -38,6 +41,8 @@ class HoldStrategy(IStrategy):
 
     startup_candle_count: int = 200
 
+    custom_info = {}
+
     order_types = {
         'entry': 'limit',
         'exit': 'limit',
@@ -57,28 +62,43 @@ class HoldStrategy(IStrategy):
                 "method": "StoplossGuard",
                 "lookback_period_candles": 96,
                 "trade_limit": 2,
-                "stop_duration_candles": 96,
+                "stop_duration_candles": 8,
                 "required_profit": 0.0,
                 "only_per_pair": False,
                 "only_per_side": False
             }
         ]
 
-
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
 
-        dataframe['atr'] = ta.ATR(dataframe)
+        dataframe = dataframe[dataframe.date >= '2023-01-01']
+        dataframe_ = dataframe.copy()
+        for c in range(1,4):
+            X = dataframe_['close'].values.reshape(-1,1)
+            kmeans = KMeans(n_clusters=3, random_state=42).fit(X)
+            dataframe_[f'label_{c}'] = kmeans.predict(X)
+            dataframe[f'label_{c}'] = dataframe_[f'label_{c}'].astype(str)
+            labels_sorted = dataframe[[f'label_{c}','close']].groupby(f'label_{c}').min()
+            labels_sorted = labels_sorted.sort_values(by=['close']).reset_index().to_dict('index')
+            labels_sorted = {value[f'label_{c}']:f'cluster_{key}' for key, value in labels_sorted.items()}
+            dataframe = dataframe.replace({f'label_{c}':labels_sorted})
+            X = dataframe_.index.values.reshape(-1,1)
+            y = dataframe_.close.values
+            model = LinearRegression()
+            model.fit(X, y)
+            dataframe[f'label_{c}_coef'] = model.coef_[0]
+            dataframe_ = dataframe_[dataframe_[f'label_{c}']==dataframe_[f'label_{c}'].iat[-1]]
 
         return dataframe
 
     def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
 
+        c_max = dataframe.groupby(['label_3']).max().at['cluster_0','close']
+
         dataframe.loc[
             (
-                (dataframe['close'] > dataframe['close'].shift(1)) &
-                (dataframe['close'].shift(1) < dataframe['close'].shift(2)) &
-                (dataframe['close'].shift(2) < dataframe['close'].shift(3)) &
-                (dataframe['close'].shift(3) < dataframe['close'].shift(4))
+                (dataframe['label_2'] == 'cluster_2') &
+                (qtpylib.crossed_above(dataframe['close'], c_max))
             ),
             'enter_long'
         ] = 1
@@ -86,16 +106,6 @@ class HoldStrategy(IStrategy):
         return dataframe
 
     def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-
-        dataframe.loc[
-            (
-                (dataframe['close'] < dataframe['close'].shift(1)) &
-                (dataframe['close'].shift(1) > dataframe['close'].shift(2)) &
-                (dataframe['close'].shift(2) > dataframe['close'].shift(3)) &
-                (dataframe['close'].shift(3) > dataframe['close'].shift(4))
-            ),
-            'exit_long'
-        ] = 1
 
         return dataframe
 
@@ -111,9 +121,11 @@ class HoldStrategy(IStrategy):
                             **kwargs) -> float:
 
         dataframe, _ = self.dp.get_analyzed_dataframe(pair=pair, timeframe=self.timeframe)
-        previous_candle = dataframe.iloc[-2].squeeze()
-        risk = previous_candle['atr'] / current_rate * 2
+        c_min = dataframe.groupby(['label_3']).min().at['cluster_0','close']
+        risk = 1 - c_min / current_rate
         stake = self.position_size(max_stake, risk)
+
+        self.custom_info[pair] = {'init_c_min':c_min, 'init_risk':risk}
 
         return stake
     
@@ -141,11 +153,11 @@ class HoldStrategy(IStrategy):
 
         dataframe, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
         trade_date = timeframe_to_prev_date(self.timeframe, trade.open_date_utc)
-        trade_dataframe = dataframe.loc[dataframe['date'] <= trade_date]
-        prev_trade_candle = trade_dataframe.iloc[-2].squeeze()
+
+        c_min = self.custom_info[pair]['init_c_min']
 
         return stoploss_from_absolute(
-            trade.open_rate - prev_trade_candle['atr'] * 2,
+            c_min,
             trade.open_rate,
             is_short=trade.is_short,
             leverage=trade.leverage
