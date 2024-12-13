86cac42c4601c40b340af6cdca16a42d4ce05da6
mohsenjfar <mohsen.j.far@gmail.com>
2024-12-13 16:18:46 +0000

    update

--- user_data/strategies/strategy.py
+++ user_data/strategies/strategy.py
@@ -72,23 +72,19 @@ class Strategy(IStrategy):
         return pd.concat((bid_dataframe,ask_dataframe))
 
 
-    def correlated_ticker(self, ticker, thresh=0.9):
-        path = "user_data/data/bybit/futures"
-        all_files = os.listdir(path)
-        files = [file for file in all_files if '_USDT_USDT-15m-futures' in file]
-        tickers = {file[:-30]:pd.read_feather(path + "/" + file).close for file in files}
-        dataframe = pd.DataFrame(tickers).drop(['BTC','ETH'], axis=1).ffill()
-        corr_df = dataframe.corr()
-        if ticker in corr_df.columns:
-            corr_df = corr_df[(corr_df > thresh) & (corr_df != 1)].dropna(how='all', axis=0).dropna(how='all', axis=1)
-            return corr_df[ticker][corr_df[ticker] == corr_df[ticker].max()].to_dict()
-        else:
-            return np.nan
+    def correlated_pair(self, pair, thresh=0.9):
+        pairs = self.dp.current_whitelist()
+        tickers = {p:self.dp.get_pair_dataframe(pair=p, timeframe=self.timeframe).close for p in pairs}
+        dataframe = pd.DataFrame(tickers).ffill()
+        dataframe.to_csv(f'user_data/notebooks/corr_df.csv', index=False)
+        series = dataframe.corr()[pair].drop(pair)
+        series = series[(series == series.max()) & (series > thresh)]
+        return series.index[0] if series.size > 0 else np.nan
 
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
 
-        dataframe["corr_ticker"] = self.correlated_ticker(metadata['pair'])
+        dataframe["corr_pair"] = self.correlated_pair(metadata['pair'], thresh=0.8)
         dataframe["rsi"] = ta.RSI(dataframe, timeperiod=14)
         dataframe['tema'] = ta.TEMA(dataframe, timeperiod=9)
 
@@ -116,7 +112,7 @@ class Strategy(IStrategy):
         open_trade = Trade.get_trades_proxy(is_open=True)
         if open_trade:
             trade = open_trade[-1]
-            if trade.get_custom_data(key='corr_ticker') in metadata['pair']:
+            if trade.get_custom_data(key='corr_pair') == metadata['pair']:
                 if trade.is_short:
                     dataframe['enter_long'] = 1
                     dataframe['enter_short'] = 0
@@ -134,7 +130,7 @@ class Strategy(IStrategy):
                 (dataframe['rsi_second_last_min'] < dataframe['rsi_last_min']) & # Guard
                 (dataframe['rsi_second_last_min'] < 50) & # Guard
                 (dataframe['rsi_last_min'] < 50) & # Guard
-                (dataframe["corr_ticker"].notna()) & # Guard
+                (dataframe["corr_pair"].notna()) & # Guard
                 (qtpylib.crossed_above(dataframe['close'], dataframe['tema'])) # Trigger
             ),
             'enter_long'
@@ -146,11 +142,13 @@ class Strategy(IStrategy):
                 (dataframe['rsi_second_last_max'] > dataframe['rsi_last_max']) & # Guard
                 (dataframe['rsi_second_last_max'] > 50) & # Guard
                 (dataframe['rsi_last_max'] > 50) & # Guard
-                (dataframe["corr_ticker"].notna()) & # Guard
+                (dataframe["corr_pair"].notna()) & # Guard
                 (qtpylib.crossed_below(dataframe['close'], dataframe['tema'])) # Trigger
             ),
             'enter_short'
         ] = 1
+
+        dataframe.to_csv(f'user_data/notebooks/{metadata['pair'][:-10]}_df.csv', index=False)
 
         return dataframe
 
@@ -208,7 +206,7 @@ class Strategy(IStrategy):
             current_candle = dataframe.iloc[-1].squeeze()
             risk = current_candle.short_risk if trade.is_short else current_candle.long_risk
             trade.set_custom_data(key='risk', value=risk)
-            trade.set_custom_data(key='corr_ticker', value=current_candle.corr_ticker)
+            trade.set_custom_data(key='corr_pair', value=current_candle.corr_pair)
 
             trade.set_custom_data(key='OB', value=self.dp.orderbook(pair=pair, maximum=200))
             
