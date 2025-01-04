4001a5263da1a441520afb287ee3a10e3be311ac
mohsenjfar <mohsen.j.far@gmail.com>
2025-01-04 15:37:37 +0000

    update

--- user_data/strategies/strategy.py
+++ user_data/strategies/strategy.py
@@ -1,25 +1,20 @@
-from pandas import DataFrame
+# importing freqtrade modules
 from freqtrade.persistence import Trade
-from datetime import datetime, timedelta, timezone
-from typing import Optional
-from technical import qtpylib
+from freqtrade.strategy import IStrategy, stoploss_from_absolute
+
+# importing calculation modules
 import pandas as pd
-import math
-import os
-from typing import Dict
+from pandas import DataFrame
 import numpy as np
-import talib.abstract as ta
+from scipy import stats
 from scipy.signal import argrelextrema
-from freqtrade.strategy import (
-    IStrategy,
-    stoploss_from_open,
-    stoploss_from_absolute,
-    timeframe_to_prev_date
-)
+
+# importing other modules
+from datetime import datetime, timezone
+from typing import Optional
 import logging
 
 logger = logging.getLogger(__name__)
-
 
 class Strategy(IStrategy):
 
@@ -37,9 +32,9 @@ class Strategy(IStrategy):
 
     startup_candle_count: int = 48
 
-    process_only_new_candles = True
+    process_only_new_candles = False
 
-    kernel = 1
+    kernel, score = 1, 4
 
     order_types = {
         'entry': 'limit',
@@ -56,19 +51,16 @@ class Strategy(IStrategy):
 
     def ob_dataframe(self, pair):
         ob = self.dp.orderbook(pair, maximum=200)
-        bid_values = {
-            'price': np.array(ob['bids'])[:,0],
-            'volume': np.array(ob['bids'])[:,1],
-            'side':'bid'
+        return {
+            'bids' : {
+                'price': np.array(ob['bids'])[:,0],
+                'volume': np.array(ob['bids'])[:,1],
+            },
+            'asks': {
+                'price': np.array(ob['asks'])[:,0],
+                'volume': np.array(ob['asks'])[:,1],
+            }
         }
-        ask_values = {
-            'price': np.array(ob['asks'])[:,0],
-            'volume': np.array(ob['asks'])[:,1],
-            'side':'ask'
-        }
-        bid_dataframe = pd.DataFrame(bid_values)
-        ask_dataframe = pd.DataFrame(ask_values)
-        return pd.concat((bid_dataframe,ask_dataframe))
     
 
     def informative_pairs(self):
@@ -76,17 +68,40 @@ class Strategy(IStrategy):
         return [(pair, '1w') for pair in pairs]
 
 
+    def add_min_one(self, num, add=True):
+        side = 1 if add else -1
+        return float(str(num)[:-1] + str(int(str(num)[-1]) + 1 * side))
+
+
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
 
         informative = self.dp.get_pair_dataframe(pair=metadata['pair'], timeframe='1w')
+        ob = self.dp.orderbook(metadata['pair'], maximum=200)
+
         min_peaks = argrelextrema(informative["low"].values, np.less_equal, order=self.kernel)
         max_peaks = argrelextrema(informative["high"].values, np.greater_equal, order=self.kernel)
         informative.loc[(informative.index.isin(min_peaks[0])),'extrema'] = informative.low
         informative.loc[(informative.index.isin(max_peaks[0])),'extrema'] = informative.high
-        dataframe['last_peak'] = informative.extrema.dropna().iloc[-1]
-        dataframe['last_peak_diff'] = informative.extrema.dropna().pct_change().iloc[-1]
         bins = informative.extrema.dropna().sort_values().values
-        dataframe['boundries'] = pd.cut(dataframe.close, bins=bins)
+        dataframe['boundaries'] = pd.cut(dataframe.close, bins=bins)
+        dataframe['left'] = dataframe.boundaries.apply(lambda x: x.left)
+        dataframe['right'] = dataframe.boundaries.apply(lambda x: x.right)
+    
+        bids = pd.DataFrame(np.array(ob['bids']), columns=['price','volume'])
+        short_price = bids[abs(stats.zscore(bids.volume)) > self.score].price.values[-1]
+        dataframe['short_price'] = self.add_min_one(short_price, add=False)
+        dataframe['short_stop'] = self.add_min_one(short_price)
+        dataframe['short_risk'] = abs(1 - dataframe['short_stop'] / dataframe['short_price'])
+        dataframe['bids_max'] = bids.price.max()
+        dataframe['bids_min'] = bids.price.min()
+        
+        asks = pd.DataFrame(np.array(ob['asks']), columns=['price','volume'])
+        long_price = asks[abs(stats.zscore(asks.volume)) > self.score].price.values[-1]
+        dataframe['long_price'] = self.add_min_one(long_price)
+        dataframe['long_stop'] = self.add_min_one(long_price, add=False)
+        dataframe['long_risk'] = abs(1 - dataframe['long_price'] / dataframe['long_stop'])
+        dataframe['asks_max'] = asks.price.max()
+        dataframe['asks_min'] = asks.price.min()
 
         return dataframe
 
@@ -95,16 +110,16 @@ class Strategy(IStrategy):
 
         dataframe.loc[
             (
-                (dataframe['last_peak_diff'] < 0) &
-                (dataframe['close'] > dataframe['last_peak'])
+                (dataframe['left'] > dataframe['bids_min']) &
+                (dataframe['left'] < dataframe['bids_max'])
             ),
             'enter_long'
         ] = 1
 
         dataframe.loc[
             (
-                (dataframe['last_peak_diff'] > 0) &
-                (dataframe['close'] < dataframe['last_peak'])
+                (dataframe['right'] > dataframe['asks_min']) &
+                (dataframe['right'] < dataframe['asks_max'])
             ),
             'enter_short'
         ] = 1
@@ -114,6 +129,22 @@ class Strategy(IStrategy):
 
     def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
 
+        dataframe.loc[
+            (
+                (dataframe['right'] > dataframe['asks_min']) &
+                (dataframe['right'] < dataframe['asks_max'])
+            ),
+            'exit_long'
+        ] = 1
+
+        dataframe.loc[
+            (
+                (dataframe['left'] > dataframe['bids_min']) &
+                (dataframe['left'] < dataframe['bids_max'])
+            ),
+            'exit_short'
+        ] = 1
+
         return dataframe
 
 
@@ -121,16 +152,11 @@ class Strategy(IStrategy):
                             proposed_stake: float, min_stake: Optional[float], max_stake: float,
                             leverage: float, entry_tag: Optional[str], side: str,
                             **kwargs) -> float:
-
-        candle_open_date = timeframe_to_prev_date(self.timeframe, current_time)
-        if candle_open_date + timedelta(seconds=5) < current_time:
-            return None
         
         try:
             dataframe, _ = self.dp.get_analyzed_dataframe(pair=pair, timeframe=self.timeframe)
-            last_candle = dataframe.iloc[-1].squeeze()
-            limit = last_candle.boundries.right if side == 'short' else last_candle.boundries.left
-            risk = abs(1 - last_candle.close / limit)
+            current_candle = dataframe.iloc[-1].squeeze()
+            risk = current_candle.short_risk if side == 'short' else current_candle.long_risk
             today = datetime.now(timezone.utc).date()
             closed_trades = Trade.get_trades_proxy(close_date=today)
             today_loss = sum(trade.close_profit_abs for trade in closed_trades if trade.close_profit_abs < 0)
@@ -145,7 +171,7 @@ class Strategy(IStrategy):
             return min((proposed_stake * (abs(self.stoploss)) / 2) / (risk * leverage), proposed_stake)
         
         except Exception as e:
-            logger.info(e)
+            self.dp.send_msg(e)
             return None
     
 
@@ -153,27 +179,27 @@ class Strategy(IStrategy):
                            entry_tag: str | None, side: str, **kwargs) -> float:
 
         dataframe, _ = self.dp.get_analyzed_dataframe(pair=pair, timeframe=self.timeframe)
-        return dataframe["close"].iat[-1]
+        current_candle = dataframe.iloc[-1].squeeze()
+        return current_candle.short_price if side == "short" else current_candle.long_price
 
 
     def order_filled(self, pair: str, trade: Trade, order, current_time: datetime, **kwargs) -> None:
 
         if (trade.nr_of_successful_entries == 1) and (order.ft_order_side == trade.entry_side):
-            trade.set_custom_data(key='OB', value=self.ob_dataframe(pair).to_dict())
-
-        return None
+            dataframe, _ = self.dp.get_analyzed_dataframe(pair=pair, timeframe=self.timeframe)
+            current_candle = dataframe.iloc[-1].squeeze()
+            stop = current_candle.short_stop if trade.is_short else current_candle.long_stop
+            trade.set_custom_data(key='stop', value=stop)
+            trade.set_custom_data(key='OB', value=self.dp.orderbook(pair, maximum=200))
     
 
     def custom_stoploss(self, pair: str, trade: 'Trade', current_time: datetime,
                         current_rate: float, current_profit: float, after_fill: bool, 
                         **kwargs) -> Optional[float]:
 
-        dataframe, _ = self.dp.get_analyzed_dataframe(pair=pair, timeframe=self.timeframe)
-        last_candle = dataframe.iloc[-1].squeeze()
-        limit = last_candle.boundries.right if trade.is_short else last_candle.boundries.left
-
+        stop = trade.get_custom_data(key='stop')
         return stoploss_from_absolute(
-                limit,
+                stop,
                 current_rate,
                 is_short=trade.is_short,
                 leverage=trade.leverage
