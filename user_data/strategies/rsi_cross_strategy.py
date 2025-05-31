60782334950e36f142600e9a40c8cece7602c53f
mohsenjfar <mohsen.j.far@gmail.com>
2025-05-31 11:06:24 +0000

    rsi 4h informative

--- user_data/strategies/rsi_cross_strategy.py
+++ user_data/strategies/rsi_cross_strategy.py
@@ -1,7 +1,8 @@
 from freqtrade.strategy import (
     IStrategy,
     timeframe_to_prev_date,
-    stoploss_from_absolute
+    stoploss_from_absolute,
+    informative
 )
 import freqtrade.vendor.qtpylib.indicators as qtpylib
 from pandas import DataFrame
@@ -27,6 +28,16 @@ class RSICrossStrategy(IStrategy):
     process_only_new_candles = True
 
 
+    @informative('4h')
+    def populate_indicators_inf4(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        
+        dataframe['rsi'] = ta.RSI(dataframe, 14)
+        dataframe['rsi_max_index'] = dataframe[dataframe['rsi'] > 70].index.max()
+        dataframe['rsi_min_index'] = dataframe[dataframe['rsi'] < 30].index.max()
+
+        return dataframe
+
+
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
 
         dataframe['rsi'] = ta.RSI(dataframe['close'], timeperiod=14)
@@ -38,12 +49,16 @@ class RSICrossStrategy(IStrategy):
         
         dataframe.loc[
             (
-                qtpylib.crossed_above(dataframe['rsi'], 30)
+                qtpylib.crossed_above(dataframe['rsi'], 30) &
+                (dataframe['rsi_min_index_4h'] > dataframe['rsi_max_index_4h'] ) &
+                (dataframe['rsi_4h'] > 30 )
             ), "enter_long"] = 1
     
         dataframe.loc[
             (
-                qtpylib.crossed_below(dataframe['rsi'], 70)
+                qtpylib.crossed_below(dataframe['rsi'], 70) &
+                (dataframe['rsi_max_index_4h'] > dataframe['rsi_min_index_4h'] ) &
+                (dataframe['rsi_4h'] < 70 )
             ), "enter_short"] = 1
 
         return dataframe
@@ -62,8 +77,8 @@ class RSICrossStrategy(IStrategy):
         pre_candle = dataframe.iloc[-1].squeeze()
 
         conditions = (
-            pre_candle.rsi < 30 and trade.is_short,
-            pre_candle.rsi > 70 and not trade.is_short
+            pre_candle['rsi'] < 30 and trade.is_short,
+            pre_candle['rsi'] > 70 and not trade.is_short
         )
 
         if any(conditions):
