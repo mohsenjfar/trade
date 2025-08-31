6fa7f1c82b9c076169f38f3acf8b1d92a6aadefc
mohsenjfar <mohsen.j.far@gmail.com>
2025-08-31 23:38:02 +0330

    update

--- user_data/strategies/strategy.py
+++ user_data/strategies/strategy.py
@@ -166,7 +166,7 @@ class Strategy(IStrategy):
                 (dataframe['volume'] > 0) & # Guard
                 (dataframe['do_predict'] == 1) & # Guard
                 (dataframe['&s-up_or_down'] == 'down') & # Guard
-                (qtpylib.crossed_above(dataframe['rsi'], self.short_rsi.value)) # Trigger
+                (qtpylib.crossed_below(dataframe['rsi'], self.short_rsi.value)) # Trigger
             ),
             'enter_short'] = 1
 
