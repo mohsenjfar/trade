b80a41473dc896710d38062ee136de9dd5d5f647
mohsenjfar <mohsen.j.far@gmail.com>
2025-11-26 20:10:58 +0330

    update

--- user_data/strategies/strategy.py
+++ user_data/strategies/strategy.py
@@ -1,8 +1,8 @@
 import freqtrade.vendor.qtpylib.indicators as qtpylib
 from pandas import DataFrame
-import talib.abstract as ta
 import numpy as np
 import pandas as pd
+import talib.abstract as ta
 from freqtrade.persistence import Trade
 from freqtrade.strategy import (
     IStrategy,
@@ -12,7 +12,7 @@ from freqtrade.strategy import (
 from datetime import datetime
 from typing import Optional
 
-class RSIBreak(IStrategy):
+class RSICross(IStrategy):
 
     INTERFACE_VERSION = 3
 
@@ -20,7 +20,7 @@ class RSIBreak(IStrategy):
 
     trade_max_loss_allowed = 0.005
 
-    timeframe = '15m'
+    timeframe = '5m'
 
     can_short: bool = True
 
@@ -30,7 +30,7 @@ class RSIBreak(IStrategy):
 
     use_custom_stoploss = True
 
-    def extract_features(self, df, c1, c2, e, name):
+    def extract_features(self, df, c1, c2, e, col, name):
 
         highs = df.loc[c1].reset_index().rename(columns={'index':'up_index'})
         crosses = df.loc[c2].reset_index().rename(columns={'index':'down_index'})
@@ -52,9 +52,9 @@ class RSIBreak(IStrategy):
         intervals = pd.IntervalIndex.from_arrays(pairs['up_index'], pairs['down_index'], closed='both')
         df['range_id'] = pd.cut(df.index, intervals)
 
-        groups = df.groupby('range_id', observed=True)[e]
-        values = groups.max() if e == "high" else groups.min()
-        values = values.reset_index().rename(columns={e: name})
+        groups = df.groupby('range_id', observed=True)[col]
+        values = groups.max() if e == "max" else groups.min()
+        values = values.reset_index().rename(columns={col: name})
         values['index'] = intervals.left
 
         df = df.merge(values, left_on=df.index, right_on='index', how='left')
@@ -65,29 +65,29 @@ class RSIBreak(IStrategy):
 
         dataframe['rsi'] = ta.RSI(dataframe['close'], timeperiod=14)
 
-        c1 = qtpylib.crossed_above(dataframe['rsi'], 70)
-        c2 = qtpylib.crossed_below(dataframe['rsi'], 70)
-        dataframe = self.extract_features(dataframe, c1, c2, 'high', "max_high")
+        c1_over = qtpylib.crossed_above(dataframe['rsi'], 70)
+        c2_over = qtpylib.crossed_below(dataframe['rsi'], 70)
 
-        c1 = qtpylib.crossed_below(dataframe['rsi'], 30)
-        c2 = qtpylib.crossed_above(dataframe['rsi'], 30)
-        dataframe = self.extract_features(dataframe, c1, c2, 'low', "min_low")
+        dataframe = self.extract_features(dataframe, c1_over, c2_over, 'max', "high", "max_high")
+        dataframe = self.extract_features(dataframe, c1_over, c2_over, 'max', "rsi", "max_rsi")
+        dataframe = self.extract_features(dataframe, c2_over, c1_over, 'min', 'low', "sl")
+        dataframe = self.extract_features(dataframe, c2_over, c1_over, 'min', 'rsi', "sl_rsi")
+        
+        c1_under = qtpylib.crossed_below(dataframe['rsi'], 30)
+        c2_under = qtpylib.crossed_above(dataframe['rsi'], 30)
+
+        dataframe = self.extract_features(dataframe, c1_under, c2_under, 'min', "low", "min_low")
+        dataframe = self.extract_features(dataframe, c1_under, c2_under, 'min', "rsi", "min_rsi")
+        dataframe = self.extract_features(dataframe, c2_under, c1_under, 'max', "high", "ss")
+        dataframe = self.extract_features(dataframe, c2_under, c1_under, 'max', "rsi", "ss_rsi")
 
         dataframe.loc[dataframe['max_high'].notna(),"cat"] = 'H'
         dataframe.loc[dataframe['min_low'].notna(),"cat"] = 'L'
 
-        c1 = dataframe['max_high'].notna()
-        c2 = qtpylib.crossed_above(dataframe['close'], dataframe['max_high'].ffill())
-        dataframe = self.extract_features(dataframe, c1, c2, 'low', "sl")
-
-        c1 = dataframe['min_low'].notna()
-        c2 = qtpylib.crossed_below(dataframe['close'], dataframe['min_low'].ffill())
-        dataframe = self.extract_features(dataframe, c1, c2, 'high', "ss")
-        
         return dataframe
 
+    @informative('15m')
     @informative('1h')
-    @informative('4h')
     def populate_indicators_(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
 
         dataframe = self.populate_features(dataframe)
@@ -96,7 +96,17 @@ class RSIBreak(IStrategy):
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
 
+        dataframe = self.freqai.start(dataframe, metadata, self)
+        
         dataframe = self.populate_features(dataframe)
+
+        indicies = dataframe[dataframe['max_high'].notna()].index
+        dataframe['sb'] = dataframe['max_high'].iat[indicies[-2]]
+        dataframe['iss'] = dataframe.iloc[indicies[-2]:].high.max()
+
+        indicies = dataframe[dataframe['min_low'].notna()].index
+        dataframe['lb'] = dataframe['min_low'].iat[indicies[-2]]
+        dataframe['isl'] = dataframe.iloc[indicies[-2]:].low.min()
 
         return dataframe
 
@@ -104,29 +114,13 @@ class RSIBreak(IStrategy):
 
         dataframe.loc[
             (
-                (dataframe['close'] > dataframe['min_low_1h'].ffill()) & # Guard
-                (qtpylib.crossed_above(dataframe['close'], dataframe['ss'].ffill())) # Trigger
-            ), ["enter_long", "enter_tag"]] = (1,"15m")
-        
-        dataframe.loc[
-            (
-                (dataframe['close'] > dataframe['min_low_4h'].ffill()) & # Guard
-                (qtpylib.crossed_above(dataframe['close'], dataframe['ss_1h'].ffill())) # Trigger
-            ), ["enter_long", "enter_tag"]] = (1,"1h")
+                (qtpylib.crossed_above(dataframe['close'], dataframe['lb']))
+            ), "enter_long"] = 1
 
         dataframe.loc[
             (
-                (dataframe['close'] < dataframe['max_high_1h'].ffill()) & # Guard
-                (qtpylib.crossed_below(dataframe['close'], dataframe['sl'].ffill())) # Trigger
-            ), ["enter_short", "enter_tag"]] = (1,"15m")
-        
-        dataframe.loc[
-            (
-                (dataframe['close'] < dataframe['max_high_4h'].ffill()) & # Guard
-                (qtpylib.crossed_below(dataframe['close'], dataframe['sl_1h'].ffill())) # Trigger
-            ), ["enter_short", "enter_tag"]] = (1,"1h")
-        
-        dataframe.to_feather("user_data/notebooks/df.feather")
+                (qtpylib.crossed_below(dataframe['close'], dataframe['sb']))
+            ), "enter_short"] = 1
 
         return dataframe
 
@@ -142,71 +136,59 @@ class RSIBreak(IStrategy):
         dataframe, _ = self.dp.get_analyzed_dataframe(pair=pair, timeframe=self.timeframe)
         last_candle = dataframe.iloc[-1].squeeze()
         total_stake = max_stake + Trade.total_open_trades_stakes()
-        stop = last_candle.high if side == "short" else last_candle.low
-        risk = abs(stop / last_candle.close - 1)
+        stop = last_candle["iss"] if side == "short" else last_candle["isl"]
+        risk = abs(stop / last_candle["close"] - 1)
         return min(total_stake * self.trade_max_loss_allowed / risk, max_stake)
 
     def custom_entry_price(self, pair: str, trade: Trade | None, current_time: datetime, proposed_rate: float,
                            entry_tag: str | None, side: str, **kwargs) -> float:
 
         dataframe, _ = self.dp.get_analyzed_dataframe(pair=pair, timeframe=self.timeframe)
+        last_candle = dataframe.iloc[-1].squeeze()
+
+        stop = last_candle['iss'] if trade.is_short else last_candle['isl']
+        trade.set_custom_data(key='stop', value=stop)
+        risk = abs(stop / last_candle["close"] - 1)
+        self.dp.send_msg(f"Trade risk ({pair}): {risk * 100:.2f} %")
+        
+        trigger = 'max_high' if trade.is_short else 'min_low'
+        index = dataframe[dataframe[trigger].notna()].index[-2]
+        trade.set_custom_data(key='index', value=index)
+        
         return dataframe['close'].iat[-1]
 
     def custom_stoploss(self, pair: str, trade: 'Trade', current_time: datetime,
                         current_rate: float, current_profit: float, after_fill: bool, 
                         **kwargs) -> Optional[float]:
 
-        dataframe_, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
-        dataframe = dataframe_.copy().ffill()
-        last_candle = dataframe.iloc[-1].squeeze()
-        
-        if trade.is_short:
-            if last_candle.ss_1h < trade.open_rate:
-                stop = last_candle.ss_1h
-            elif last_candle.ss_15m < trade.open_rate:
-                stop = last_candle.ss_15m
-            else:
-                stop = last_candle.high
-            
-            if trade.get_custom_data(key='risk') is None:
-                risk = abs(stop / last_candle.close - 1)
-                trade.set_custom_data(key='risk', value=risk)
-                self.dp.send_msg(f"Trade risk ({pair}): {risk * 100:.2f} %")
+        if current_profit > 1: return 0.3
 
-            return stoploss_from_absolute(
-                stop,
-                current_rate,
-                is_short=trade.is_short,
-                leverage=trade.leverage
-            )
+        dataframe, _ = self.dp.get_analyzed_dataframe(pair, self.timeframe)
+        index = trade.get_custom_data('index')
+        tfs = ['1h', '15m', '']
 
+        if trade.is_short:    
+            for tf in tfs:
+                values = dataframe.loc[
+                    (
+                        (dataframe[f'ss_{tf}'].notna()) &
+                        (dataframe.index >= index)
+                    ),f'ss_{tf}'].values
+                if values.size > 0: break
+            stop = np.append(values, trade.get_custom_data('stop')).min()
         else:
-            if last_candle.sl_1h > trade.open_rate:
-                stop = last_candle.sl_1h
-            elif last_candle.sl_15m > trade.open_rate:
-                stop = last_candle.sl_15m
-            else:
-                stop = last_candle.low
-            
-            if trade.get_custom_data(key='risk') is None:
-                risk = abs(stop / last_candle.close - 1)
-                trade.set_custom_data(key='risk', value=risk)
-                self.dp.send_msg(f"Trade risk ({pair}): {risk * 100:.2f} %")
+            for tf in tfs:
+                values = dataframe.loc[
+                    (
+                        (dataframe[f'sl_{tf}'].notna()) &
+                        (dataframe.index >= index)
+                    ),f'sl_{tf}'].values
+                if values.size > 0: break
+            stop = np.append(values, trade.get_custom_data('stop')).max()
 
-            return stoploss_from_absolute(
-                stop,
-                current_rate,
-                is_short=trade.is_short,
-                leverage=trade.leverage
-            )
-    
-    def custom_exit(self, pair: str, trade: Trade, current_time: datetime, 
-                    current_rate: float, current_profit: float, **kwargs) -> str:
-
-        risk = trade.get_custom_data(key='risk')
-        trade_duration = (current_time - trade.open_date_utc).seconds / 60
-        conditions = (
-            (trade_duration > 240) and (current_profit < 0),
-            (trade_duration > 1440) and (current_profit < risk * 2)
+        return stoploss_from_absolute(
+            stop,
+            current_rate,
+            is_short=trade.is_short,
+            leverage=trade.leverage
         )
-        if any(conditions): return "Trade expired!"
