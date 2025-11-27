0e82a9c68ac8a3649fe8ce1e1db86bfcbf0d3bb6
mohsenjfar <mohsen.j.far@gmail.com>
2025-11-27 04:17:43 +0330

    update

--- user_data/strategies/rsi_break.py
+++ user_data/strategies/rsi_break.py
@@ -146,12 +146,12 @@ class RSIBreak(IStrategy):
         dataframe, _ = self.dp.get_analyzed_dataframe(pair=pair, timeframe=self.timeframe)
         last_candle = dataframe.iloc[-1].squeeze()
 
-        stop = last_candle['iss'] if trade.is_short else last_candle['isl']
+        stop = last_candle['iss'] if side=='short' else last_candle['isl']
         trade.set_custom_data(key='stop', value=stop)
         risk = abs(stop / last_candle["close"] - 1)
         self.dp.send_msg(f"Trade risk ({pair}): {risk * 100:.2f} %")
         
-        trigger = 'max_high' if trade.is_short else 'min_low'
+        trigger = 'max_high' if side=='short' else 'min_low'
         index = dataframe[dataframe[trigger].notna()].index[-2]
         trade.set_custom_data(key='index', value=index)
         
@@ -162,7 +162,7 @@ class RSIBreak(IStrategy):
                         **kwargs) -> Optional[float]:
 
         if current_profit > 1: return 0.3
-        
+
         return stoploss_from_absolute(
             trade.get_custom_data('stop'),
             current_rate,
