d6cc1723af0c4634223f442d68ec412613ee4150
mohsenjfar <mohsen.j.far@gmail.com>
2025-12-04 08:58:08 +0330

    update

--- user_data/strategies/strategy.py
+++ user_data/strategies/strategy.py
@@ -22,7 +22,7 @@ class HybridStrategy(IStrategy):
 
     trade_max_loss_allowed = 0.01
 
-    timeframe = '5m'
+    timeframe = '15m'
 
     can_short: bool = True
 
@@ -91,7 +91,7 @@ class HybridStrategy(IStrategy):
     def set_freqai_targets(self, dataframe: DataFrame, metadata: Dict, **kwargs) -> DataFrame:
 
         self.freqai.class_names = ["down", "up"]
-        dataframe['&s-up_or_down'] = np.where(dataframe["close"].shift(-12) > dataframe["close"], 'up', 'down')
+        dataframe['&s-up_or_down'] = np.where(dataframe["close_4h"].shift(-4) > dataframe["close"], 'up', 'down')
 
         return dataframe
 
@@ -151,8 +151,8 @@ class HybridStrategy(IStrategy):
 
         return dataframe
 
-    @informative('15m')
     @informative('1h')
+    @informative('4h')
     def populate_indicators_(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
 
         dataframe = self.populate_features(dataframe)
@@ -179,7 +179,6 @@ class HybridStrategy(IStrategy):
             (
                 (dataframe['do_predict'] == 1) & # Guard
                 (dataframe['&s-up_or_down'] == 'up') & # Guard
-                (dataframe['cat_15m'].ffill() == "L") & # Guard
                 (qtpylib.crossed_above(dataframe['close'], dataframe['lb'])) # Trigger
             ), "enter_long"] = 1
 
@@ -187,7 +186,6 @@ class HybridStrategy(IStrategy):
             (
                 (dataframe['do_predict'] == 1) & # Guard
                 (dataframe['&s-up_or_down'] == 'down') & # Guard
-                (dataframe['cat_15m'].ffill() == "H") & # Guard
                 (qtpylib.crossed_below(dataframe['close'], dataframe['sb'])) # Trigger
             ), "enter_short"] = 1
 
@@ -208,6 +206,23 @@ class HybridStrategy(IStrategy):
         stop = last_candle["high"] if side == "short" else last_candle["low"]
         risk = abs(stop / current_rate - 1)
         return min(total_stake * self.trade_max_loss_allowed / risk, max_stake)
+
+    def confirm_trade_entry(self, pair: str, order_type: str, amount: float, rate: float,
+                            time_in_force: str, current_time: datetime, entry_tag: Optional[str],
+                            side: str, **kwargs) -> bool:
+
+        closed_trades = Trade.get_trades_proxy(is_open=False)
+        last_trade_index = closed_trades[-1].get_custom_data('index')
+        
+        dataframe, _ = self.dp.get_analyzed_dataframe(pair=pair, timeframe=self.timeframe)
+        
+        trigger = 'max_high' if side == 'short' else 'min_low'
+        index = dataframe[dataframe[trigger].notna()].index[-2]
+
+        if last_trade_index == index:
+            return False
+
+        return True
 
     def custom_stoploss(self, pair: str, trade: 'Trade', current_time: datetime,
                         current_rate: float, current_profit: float, after_fill: bool, 
@@ -236,7 +251,7 @@ class HybridStrategy(IStrategy):
             trade.set_custom_data(key='stop', value=stop)
 
         index = trade.get_custom_data('index')
-        tfs = ['_1h', '_15m']
+        tfs = ['_4h', '_1h']
         if trade.is_short:    
             for tf in tfs:
                 values = dataframe.loc[
