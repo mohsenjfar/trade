d716e4ba5dd944b8fa7941ecd98ad9e9d2209596
mohsenjfar <mohsen.j.far@gmail.com>
2026-01-08 16:24:56 +0330

    update

---
+++ user_data/backtest.sh
@@ -0,0 +1,5 @@
+
+git pull
+docker-compose run --rm atlas_engine backtesting --strategy AtlasEngine --config user_data/atlas_engine_test.json --timerange 20231218- --export trades
+git add . && git commit -m "backtest" && git push
+exit
---
+++ user_data/optimize.sh
@@ -0,0 +1,2 @@
+tmux
+docker-compose run --rm atlas_engine hyperopt --hyperopt-loss SharpeHyperOptLossDaily --spaces buy sell --strategy AtlasEngine --config user_data/atlas_engine_test.json -e 1000
