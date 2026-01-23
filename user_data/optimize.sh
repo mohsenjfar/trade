a56bf1a1a08d2d99578f407de6164d2ded0872f4
mohsenjfar <mohsen.j.far@gmail.com>
2026-01-23 08:59:06 +0330

    update

--- user_data/optimize.sh
+++ user_data/optimize.sh
@@ -8,5 +8,5 @@ config="user_data/atlas_engine_test.json"
 epochs=1000
 log_file="hyperopt.log"
 
-docker-compose run --rm $service hyperopt --hyperopt-loss $func --spaces $spaces --strategy $strategy --config $config -e $epochs --analyze-per-epoch>> $log_file
+docker-compose run --rm $service hyperopt --hyperopt-loss $func --spaces $spaces --strategy $strategy --config $config -e $epochs --analyze-per-epoch #>> $log_file
 git add . && git commit -m "backtest" && git push
