4cc1130fb96545b72b019105a287d57051b3d016
mohsenjfar <mohsen.j.far@gmail.com>
2026-01-23 18:00:26 +0330

    add loss functions to optimize bash script

--- user_data/optimize.sh
+++ user_data/optimize.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 service="atlas_engine"
-func="SharpeHyperOptLossDaily"
+
+func="ShortTradeDurHyperOptLoss"
+# func="OnlyProfitHyperOptLoss"
+# func="SharpeHyperOptLoss"
+# func="SharpeHyperOptLossDaily"
+# func="SortinoHyperOptLoss"
+# func="SortinoHyperOptLossDaily"
+# func="CalmarHyperOptLoss"
+# func="MaxDrawDownHyperOptLoss"
+# func="MaxDrawDownRelativeHyperOptLoss"
+# func="MaxDrawDownPerPairHyperOptLoss"
+# func="ProfitDrawDownHyperOptLoss"
+# func="MultiMetricHyperOptLoss"
+
 spaces="buy"
 strategy="AtlasEngine"
 config="user_data/atlas_engine_test.json"
 epochs=1000
-log_file="hyperopt.log"
 
-docker-compose run --rm $service hyperopt --hyperopt-loss $func --spaces $spaces --strategy $strategy --config $config -e $epochs --analyze-per-epoch #>> $log_file
-git add . && git commit -m "backtest" && git push
+docker-compose run --rm $service hyperopt --hyperopt-loss $func --spaces $spaces --strategy $strategy --config $config -e $epochs --analyze-per-epoch
+git add . && git commit -m "optimize" && git push
