5bd27a67d5aa343f04f124422a4b050dcbc5fc6f
mohsenjfar <mohsen.j.far@gmail.com>
2026-01-30 17:40:04 +0330

    update

--- user_data/optimize.sh
+++ user_data/optimize.sh
@@ -16,17 +16,17 @@ func="ProfitDrawDownHyperOptLoss"
 # func="MultiMetricHyperOptLoss"
 # func="SuperDuperHyperOptLoss"
 
-spaces="buy allowed_loss roi"
+spaces="buy allowed_loss"
 strategy="AtlasEngine"
 config="user_data/config.json"
 epochs=1000
 timerange="20260101-"
 timeframe="5m 15m 1h 4h"
 
-# docker-compose run --rm $service download-data -c $config -t $timeframe --timerange $timerange
+docker-compose run --rm $service download-data -c $config -t $timeframe --timerange $timerange
 
 for i in {1..1}
 do
-    # docker-compose run --rm $service hyperopt --hyperopt-loss $func --spaces $spaces --strategy $strategy --config $config -e $epochs --analyze-per-epoch --timerange $timerange
+    docker-compose run --rm $service hyperopt --hyperopt-loss $func --spaces $spaces --strategy $strategy --config $config -e $epochs --analyze-per-epoch --timerange $timerange
     docker-compose run --rm $service backtesting --strategy $strategy --config $config --timerange $timerange --export trades
 done
