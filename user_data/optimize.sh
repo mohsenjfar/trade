25b0b0c703fb8a2c2e1d1b63bba8e089837a5636
mohsenjfar <mohsen.j.far@gmail.com>
2026-01-30 17:34:42 +0330

    update

--- user_data/optimize.sh
+++ user_data/optimize.sh
@@ -23,10 +23,10 @@ epochs=1000
 timerange="20260101-"
 timeframe="5m 15m 1h 4h"
 
-docker-compose run --rm $service download-data -c $config -t $timeframe --timerange $timerange
+# docker-compose run --rm $service download-data -c $config -t $timeframe --timerange $timerange
 
 for i in {1..1}
 do
-    docker-compose run --rm $service hyperopt --hyperopt-loss $func --spaces $spaces --strategy $strategy --config $config -e $epochs --analyze-per-epoch --timerange $timerange
+    # docker-compose run --rm $service hyperopt --hyperopt-loss $func --spaces $spaces --strategy $strategy --config $config -e $epochs --analyze-per-epoch --timerange $timerange
     docker-compose run --rm $service backtesting --strategy $strategy --config $config --timerange $timerange --export trades
 done
