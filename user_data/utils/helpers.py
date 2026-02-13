ec7ac8eafe7c36e7aa4bbdffb1a34c7106269218
mohsenjfar <mohsen.j.far@gmail.com>
2026-02-13 12:25:17 +0330

    Remove unused modules: custom_indicators.py, json_store.py, and token_clustering.py to streamline the project structure and eliminate deprecated code.

--- user_data/utils/helpers.py
+++ user_data/utils/helpers.py
@@ -1,8 +1,3 @@
-"""
-ماژول اندیکاتورهای تخصصی برای معاملات رمزارز
-ویژگی‌ها: اندیکاتورهای آنچین، CVD, MVRV, Dominance, و ...
-"""
-
 import numpy as np
 import pandas as pd
 from pandas import DataFrame
@@ -11,6 +6,8 @@ import freqtrade.vendor.qtpylib.indicators as qtpylib
 from typing import Dict, List, Optional, Tuple, Union
 from datetime import datetime, timedelta
 import warnings
+import os
+import json
 warnings.filterwarnings('ignore')
 
 
@@ -838,3 +835,48 @@ class CustomIndicators:
     def clear_cache(self):
         """پاک کردن کش"""
         self._cache.clear()
+
+class JsonStore:
+    def __init__(self, path, default_data=None):
+        self.path = path
+        self.default_data = default_data or {}
+
+        if not os.path.exists(self.path):
+            self.data = self.default_data.copy()
+            self._save()
+        else:
+            self._load()
+
+    def _load(self):
+        with open(self.path, 'r', encoding='utf-8') as f:
+            self.data = json.load(f)
+
+    def _save(self):
+        with open(self.path, 'w', encoding='utf-8') as f:
+            json.dump(self.data, f, ensure_ascii=False, indent=4)
+
+    def get(self, key, default=None):
+        return self.data.get(key, default)
+
+    def set(self, key, value):
+        self.data[key] = value
+        self._save()
+
+    def delete(self, key):
+        if key in self.data:
+            del self.data[key]
+            self._save()
+
+    def append(self, key, value):
+        self.data[key].append(value)
+        self._save()
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
+        self._save()
+
+    def __contains__(self, key):
+        return key in self.data
